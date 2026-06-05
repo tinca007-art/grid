901b6c42910b3e272464b363cfa7fc787074042a
Luděk Roleček <ludek@rolecek.cz>
2026-06-05 18:09:22 +0200

    Initial content

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // Odkomentujte cestu ke cvičení, na kterém pracujete:
+
+  // 12-column grid
+  root: './01-Grid-12-columns/01-Basic-grid',
+  // root: './01-Grid-12-columns/02-Responsive-grid',
+
+  // CSS Grid 1
+  // root: './02-CSS-Grid-1/01-Grid',
+  // root: './02-CSS-Grid-1/02-Holy-grail',
+  // root: './02-CSS-Grid-1/03-Kalendar',
+  // root: './02-CSS-Grid-1/04-Object-fit',
+  // root: './02-CSS-Grid-1/05-Galerie',
+  // root: './02-CSS-Grid-1/06-Grid-cardio-1',
+  // root: './02-CSS-Grid-1/07-Grid-cardio-2/grid1',
+  // root: './02-CSS-Grid-1/07-Grid-cardio-2/grid2',
+  // root: './02-CSS-Grid-1/07-Grid-cardio-2/grid3',
+  // root: './02-CSS-Grid-1/08-Grid-cardio-3/layout1',
+  // root: './02-CSS-Grid-1/08-Grid-cardio-3/layout2',
+  // root: './02-CSS-Grid-1/08-Grid-cardio-3/layout3',
+  // root: './02-CSS-Grid-1/08-Grid-cardio-3/layout4',
+  // root: './02-CSS-Grid-1/08-Grid-cardio-3/layout5',
+
+  // CSS Grid 2
+  // root: './03-CSS-Grid-2/01-Zarovnavani',
+  // root: './03-CSS-Grid-2/02-Auto-grid',
+  // root: './03-CSS-Grid-2/03-Pojmenovane-linie',
+  // root: './03-CSS-Grid-2/04-Pojmenovane-oblasti',
+  // root: './03-CSS-Grid-2/05-Minmax',
+  // root: './03-CSS-Grid-2/06-Autofill-a-autofit',
+})
